19698795e141f3350d12bd0ac9e74e2af2e80f9a
Alexandre Cardoso <1alexandruu@gmail.com>
2025-09-10 15:35:57 +0100

    app final

--- app.py
+++ app.py
@@ -1,38 +1,33 @@
+# app.py
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
 
-car_data = pd.read_csv('vehicles.csv')  # lendo os dados
-fig = px.histogram(car_data, x="odometer")  # criar um histograma
-fig.show()  # exibindo
-
-car_data = pd.read_csv('vehicles.csv')  # lendo os dados
-# criar um gráfico de dispersão
-fig = px.scatter(car_data, x="odometer", y="price")
-fig.show()  # exibindo
-
-st.write('Análise Exploratória de Dados')
-st.write(
-    'Este aplicativo realiza uma análise exploratória de dados no conjunto de dados de anúncios de vendas de carros.')
-
-st.dataframe(car_data, height=600)  # exibir o dataframe
-
-hist_button = st.button('Criar histograma')  # criar um botão
-
-if hist_button:  # se o botão for clicado
-    # escrever uma mensagem
-    st.write(
-        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-
-    # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
+df = pd.read_csv('vehicles.csv')
 
 
-# criar uma caixa de seleção
-build_histogram = st.checkbox('Criar um histograma')
+def main():
+    st.title("Projeto Sprint 5")
+    st.markdown("Sprint 5 — Ferramentas de Desenvolvimento de Software")
+    st.markdown(
+        "Análise Exploratória de Dados no conjunto de dados de anúncios de vendas de carros.")
 
-if build_histogram:  # se a caixa de seleção for selecionada
-    st.write('Criando um histograma para a coluna odometer')
+    # quick info
+    st.dataframe(df.head())
+
+    st.header("Histograma")
+
+    if st.button("Criar histograma"):
+        fig = px.histogram(df, x="odometer")
+        st.plotly_chart(fig)
+
+    st.header("Grafico de Dispersão")
+
+    if st.checkbox("Mostrar Grafico de Dispersão"):
+        fig2 = px.scatter(df, x='price', y='odometer', color='paint_color',
+                          title=f"Dispersão — price vs odometer")
+        st.plotly_chart(fig2)
+
+
+if __name__ == "__main__":
+    main()
